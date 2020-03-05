635aa953c375b9c11c4deae4dc55c2500a8abc65
luiz otavio amboni <luiz.otavioamboni@gmail.com>
2020-03-04 23:42:44 -0300

    Inserído resolução do exercício proposto na aula 14

--- aula14Ex/ex016/script.js
+++ aula14Ex/ex016/script.js
@@ -11,9 +11,15 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
+        if (i < f){ // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else { // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
         }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
