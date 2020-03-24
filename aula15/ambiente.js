de80a23f825db76c0ef6eb4aebfa8a96e67dd594
luiz otavio amboni <luiz.otavioamboni@gmail.com>
2020-03-23 23:22:00 -0300

    Adicionado aula de introdução vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)//Incrementa valores ao array.
+num.sort()//Ordena o array em ordem alfabética.
+console.log(num)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)// Retona a posição do valor especificado dentro do array.
+console.log(`O valor 8 esta na posição: ${pos}`)
